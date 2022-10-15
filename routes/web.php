767fef029850217ebb82834febef80d65e0b7e49
ludfyrahman <ludfyr@gmail.com>
2022-10-15 07:22:17 +0700

    form and edit data user

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BackOffice\DashboardController;
+use App\Http\Controllers\BackOffice\UserController;
+use App\Http\Controllers\BackOffice\EmployeController;
+use App\Http\Controllers\BackOffice\CustomerController;
+use App\Http\Controllers\BackOffice\SupplierController;
+use App\Http\Controllers\BackOffice\ProductController;
+use App\Http\Controllers\BackOffice\ServiceController;
+use App\Http\Controllers\BackOffice\UnitController;
+use App\Http\Controllers\BackOffice\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +29,13 @@ Route::get('/', function () {
 
 
 Route::get('/dasboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('user', UserController::class);
+Route::resource('employe', EmployeController::class);
+Route::resource('product', ProductController::class);
+Route::resource('service', ServiceController::class);
+Route::resource('supplier', SupplierController::class);
+Route::resource('unit', UnitController::class);
+Route::resource('customer', CustomerController::class);
+Route::resource('type', TypeController::class);
+Route::resource('profile', TypeController::class);
+Route::resource('unint', UnitController::class);
